ad6749498e24e94010fbc3ff110e588d0980c13a
mvishok <vishokmanikantan@gmail.com>
2025-12-25 15:22:03 +0530

    Add additional role types to RoleType enum

--- src/Entity/RoleType.php
+++ src/Entity/RoleType.php
@@ -8,4 +8,10 @@ enum RoleType: string
     case BACKEND = 'backend';
     case FULLSTACK = 'fullstack';
     case DEVOPS = 'devops';
+    case MOBILE = 'mobile';
+    case AIML = 'aiml';
+    case PRODUCT = 'product';
+    case QA = 'qa';
+    case DESIGNER = 'designer';
+    case BLOCKCHAIN = 'blockchain';
 }
